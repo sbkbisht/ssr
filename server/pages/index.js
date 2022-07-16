542fb9130631e5f7d8fb3a70517f41f7ee620eec
sbkbisht <sumanbishtbk@gmail.com>
2022-07-16 13:36:50 +0530

    load and fetch data

--- server/pages/index.js
+++ server/pages/index.js
@@ -1,17 +1,26 @@
+import React from "react";
 // next js support module style
 import styles from "./index.module.css";
 import Card from "./Card";
 import data from "./API/data.json";
 
-export default () => {
-  return (
-    <div className={styles.app}>
-      <header className={styles.header}>
-        <img src="/logo.png" className={styles.logo} alt="logo" />
-      </header>
-      <div className={styles.grid}>
-        <Card /> <Card /> <Card />
+export default class Index extends React.Component {
+  static async getInitialProps() {
+    return { cards: data };
+  }
+  render() {
+    return (
+      <div className={styles.app}>
+        <header className={styles.header}>
+          <img src="/logo.png" className={styles.logo} alt="logo" />
+        </header>
+        <div className={styles.grid}>
+          {this.props.cards.map((card) => (
+            <Card key={card.id} />
+          ))}
+          <Card />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
