5faafbb59ea07cd2cf0db72feec4d8c9a479fc31
sbkbisht <sumanbishtbk@gmail.com>
2022-07-18 21:13:43 +0530

    eslint fixes

--- server/store.js
+++ server/store.js
@@ -1,47 +1,47 @@
-import { createWrapper } from 'next-redux-wrapper';
-import { createStore } from 'redux';
-import data from './pages/API/data.json';
+import { createWrapper } from "next-redux-wrapper";
+import { createStore } from "redux";
+import data from "./pages/API/data.json";
 
 // initial state
 const startState = {
-    cards: []
+  cards: [],
 };
 
 // Actions
 export const initialCards = () => {
-    return {
-        type: 'INITIALCARDS',
-        cards: data
-    }
+  return {
+    type: "INITIALCARDS",
+    cards: data,
+  };
 };
 
 export const addItem = (item) => {
-    return {
-        type: 'ADD',
-        item
-    }
+  return {
+    type: "ADD",
+    item,
+  };
 };
 
 // reducers
 export const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case 'INITIALCARDS':
-            return {
-                cards: action.cards,
-            }
-        case 'ADD':
-            return {
-                ...state,
-                cards: [...state.cards, action.item],
-            }
-        default: return state
-    }
-}
-
+  switch (action.type) {
+    case "INITIALCARDS":
+      return {
+        cards: action.cards,
+      };
+    case "ADD":
+      return {
+        ...state,
+        cards: [...state.cards, action.item],
+      };
+    default:
+      return state;
+  }
+};
 
 // create store
 const store = (initialState = startState) => {
-    return createStore(reducer, initialState);
+  return createStore(reducer, initialState);
 };
 
 export const initStore = createWrapper(store);
