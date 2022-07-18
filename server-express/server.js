d41f65b7b6984623ccfdf35eb431f32b98b5bcea
sbkbisht <sumanbishtbk@gmail.com>
2022-07-18 22:01:04 +0530

    add express routes

--- server-express/server.js
+++ server-express/server.js
@@ -12,8 +12,21 @@ const handle = app.getRequestHandler();
 
 // now state using app environment or  prepare our server, this is the process next js does
 app.prepare().then(() => {
+  // next js have prepare method
   // so once file build we create express server
   const server = express();
+  server.get("/page2", (req, res) => {
+    return app.render(req, res, "/page2");
+  }); // get is functionality to get the route and then we call this route to page2 and you have typical syntax for express (req, res)
+
+  server.get("/page3", (req, res) => {
+    return app.render(req, res, "/ohyeah");
+  });
+  // default
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
   server.listen(port, (err) => {
     if (err) throw err;
     console.log(`Ready on http://localhost:${port}`);
